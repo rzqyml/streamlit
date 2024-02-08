8227313d2eebf7fe641fe2fa7d39e84447f49fbc
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-02-08 13:57:37 +0700

    Update stream_kbst.py

--- stream_kbst.py
+++ stream_kbst.py
@@ -1,38 +1,37 @@
-import pickle
 import streamlit as st
+import pickle
 
-# MEMBACA MODEL
+# Load the Naive Bayes model
 model = pickle.load(open('kbst_model.sav', 'rb'))
 
-#TITLE WEB
-st.title('SISTEM PREDIKSI KELUARGA BERESIKO STUNTING')
-
-# fUNGSI PREDIKSI
+# Function to make predictions
 def predict(input_data):
-        prediction = model.predict([input_data])[0]
-    #return prediction
+    # Placeholder for the actual prediction logic based on your model
+    # Modify this part according to your model's input requirements
+    prediction = model.predict([input_data])[0]
+    return prediction
 
-#STREAMLIT APP
+# Streamlit app
 def main():
     st.title("Aplikasi Prediksi Stunting Keluarga")
 
-    #Input Data
+    # Input data
     st.header("Input Data")
-
+    
     sumber_air_minum_buruk = st.sidebar.number_input("Apakah Sumber Air Minum Buruk?", min_value=0, max_value=1)
     sanitasi_buruk = st.sidebar.number_input("Apakah Sanitasi Buruk?", min_value=0, max_value=1)
     terlalu_muda_istri = st.sidebar.number_input("Apakah Istri Terlalu Muda?", min_value=0, max_value=1)
     terlalu_tua_istri = st.sidebar.number_input("Apakah Istri Terlalu Tua?", min_value=0, max_value=1)
     terlalu_dekat_umur = st.sidebar.number_input("Apakah Umur Suami & Istri Terlalu Dekat?", min_value=0, max_value=1)
     terlalu_banyak_anak = st.sidebar.number_input("Apakah Memiliki Banyak Anak?", min_value=0, max_value=1)
-
+    
     input_data = [sumber_air_minum_buruk, sanitasi_buruk, terlalu_muda_istri, terlalu_tua_istri, terlalu_dekat_umur, terlalu_banyak_anak]
 
-    #Button Prediksi
-    if st.button("Test Prediksi"):
+    # Predict button
+    if st.button("Prediksi"):
         result = predict(input_data)
 
-        #Tampilan Hasil Prediksi
+        # Display result
         st.header("Hasil Prediksi")
         if result == 0:
             st.write("Keluarga Tidak Beresiko Stunting")
